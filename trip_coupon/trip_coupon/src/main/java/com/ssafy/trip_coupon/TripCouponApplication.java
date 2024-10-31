9573a7326214ff838adf455a555e54ae3295bead
최휘준 <chjcoder@gmail.com>
2024-10-31 16:13:47 +0900

    start

---
+++ trip_coupon/trip_coupon/src/main/java/com/ssafy/trip_coupon/TripCouponApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.trip_coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripCouponApplication.class, args);
+	}
+
+}
---
+++ trip_coupon/trip_coupon/src/test/java/com/ssafy/trip_coupon/TripCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.trip_coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
